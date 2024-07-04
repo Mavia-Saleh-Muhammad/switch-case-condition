4950f354c4c5d2d08c5181946588d8d04dc91614
Mavi <160594705+maviasalemuhmmad@users.noreply.github.com>
2024-07-04 08:49:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,74 @@
+// let month = "Ramadan" 
+// if (month== "ramadan"){
+//     console.log("Water bottle is  not allowed")
+// }else {
+//     console.log ("Water bottle is allowed ")
+// }
+
+
+// let raining = true 
+// let weather ="cloudy" 
+// if (raining){
+//     console.log("Take a umbrella with yourself")
+// }else if (weather == "cloudy"){
+//     console.log("no need for umbrella ");
+// }else{
+//     console.log("wear sun glass")
+// }
+
+ 
+// conditional ternary operators
+// let isHungry = true;
+
+// let snacke = isHungry ? "apple" :"water"
+
+// let isRishtaAccepted= true;
+
+// let proposal =isRishtaAccepted ?
+// "Gave Eiddi": "Moye Moye"
+
+// console.log(proposal)
+
+
+// let isRishtaAccepted = false
+// let proposal =  isRishtaAccepted ?
+// "Give Eiddi" : "Moye Moye";
+
+// if (isRishtaAccepted){
+//     proposal ="EIDI"
+// }else{
+//     proposal = "Moye Moye"
+// }console.log (proposal)
+
+
+2// switch Statements
+// let car = `Fortuner`
+// switch(car){
+//     case "Alto":
+//      console.log("pending")
+//      break;
+//      case "Civic":
+//         console.log("We will thank about it ")
+//         break;
+//      case "Fortuner":
+//         console.log("Accepted 100%");
+//         default:
+//         console.log("Work Hard")
+// }
+
+
+
+
+// let car = `Fortuner`
+// switch(car){
+//     case "Alto":
+//      console.log("pending")
+//      break;
+//      case "Civic":
+//         console.log("We will thank about it ")
+//         break;
+//      case "Fortuner":
+//         console.log("Accepted 100%");
+//         default:
+//         console.log("Work Hard")
+// }
